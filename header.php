909462418685b5cde3a303a6b76f44aeb4106f0c
Jorge Mayorga <jorge.mayorga@fortra.com>
2024-01-13 11:25:55 -0500

    [FEAT] Add meta tags in head

--- header.php
+++ header.php
@@ -12,13 +12,39 @@
 ?>
 <!doctype html>
 <html <?php language_attributes(); ?>>
+
+<!-- --------------------------------------------------- -->
+<!-- --- HEAD ------------------------------------------ -->
+<!-- --------------------------------------------------- -->
 <head>
+
+	<!-- --------------------------------------- -->
+	<!-- -- META TAGS -------------------------- -->
+	<!-- --------------------------------------- -->
+	<meta name="robots" content="follow"/>
+	<meta name="author" content="jl.mayorga.co@gmail.com" />
+	<meta name="keywords" content="Angular, JavaScript, TypeScript, React, Vue, Frontend Development, Full Stack Development, UI/UX Design, Responsive Design, Agile Methodologies, Git, HTML, CSS, Node.js, Express.js, Nest.js, RESTful APIs, MySQL, PostgreSQL, MongoDB, Serverless Architecture, Webpack, Testing Frameworks (Jasmine, Karma, Jest, Cypress), DevOps, CI/CD, Docker, AWS, Azure, Heroku, GraphQL, Laravel, Python, Django, Flask, Machine Learning, Robotic Systems, Reinforcement Learning, Control Systems, Distributed Control, Game Theory, Research, Electronic Engineering, Embedded Systems, Raspberry Pi, BeagleBone Black, Sensors, IoT, Remote Sensing, Computer Vision, PID Control, LQR Control, Optimal Control, Dynamical Systems, Replicator Dynamics, Electronic Design, Hardware Integration, Edge Computing, Edge Devices, Data Acquisition, Signal Processing, Algorithm Development, Robotics, Internet of Things (IoT), Cyber-Physical Systems, Simulink, State Machines, Finite State Machines, Real-time Systems, Feedback Control, System Identification, Kalman Filtering, Decision Support Systems, Human-Machine Interaction, Human-Robot Interaction, Autonomous Systems, Swarm Robotics, Mechatronics, Actuators, Kinematics, Dynamics, Embedded C, Firmware Development, Energy Harvesting, Wireless Communication, MQTT, Zigbee, LoRa, Bluetooth, Industrial Automation."/>
+	<meta name="description" content="Highly skilled and experienced Angular Senior Front End Developer with over 6 years of experience in developing and implementing user-friendly and visually appealing web applications using Angular and other front-end technologies. Strong proficiency in HTML, CSS, and JavaScript. Excellent understanding of responsive design, cross-browser compatibility, and accessibility standards. Proven ability to work in a fast-paced environment and collaborate effectively with cross-functional teams. Strong communication and leadership skills, with experience mentoring junior developers. Demonstrated understanding of the software development life cycle, Agile methodologies, and software development best practices."/>
+	<meta name="copyright" content="Copyright Jorge Luis Mayorga Taborda" />
+	<!-- --------------------------------------- -->
+
+	<!-- --------------------------------------- -->
+	<!-- --- META WORDPRESS --------------------- -->
+	<!-- --------------------------------------- -->
 	<meta charset="<?php bloginfo( 'charset' ); ?>">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<link rel="profile" href="https://gmpg.org/xfn/11">
+	<!-- --------------------------------------- -->
 
+	<!-- --------------------------------------- -->
+	<!-- ----WORDPRESS HEAD --------------------- -->
+	<!-- --------------------------------------- -->
 	<?php wp_head(); ?>
+	<!-- --------------------------------------- -->
+
 </head>
+<!-- ----------------------------------------------------------------- -->
+
 
 <body <?php body_class(); ?>>
 <?php wp_body_open(); ?>
