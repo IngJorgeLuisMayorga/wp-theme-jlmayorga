b521a89cb5a072e6a57446018984b1f6401699c1
Jorge Mayorga <jorge.mayorga@fortra.com>
2024-01-13 11:25:30 -0500

    Create Homepage template

---
+++ homepage.tpl.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * Template Name: Custom Homepage
+ */
+
+get_header(); ?>
+
+<div>
+  <?php    
+  if ( have_posts() ) :    
+     while ( have_posts() ) : the_post();
+       the_content();
+     endwhile;
+  endif;   
+  ?>  
+</div>
+
+<div>
+  <?php
+  $wp_query = new WP_Query(array(
+    'post_type' => 'post',
+    'post_status' => 'publish'
+  ));
+  if ( $wp_query->have_posts() ) :
+    while ( $wp_query->have_posts() ) : $wp_query->the_post();
+      the_title();
+      /* Post loop content goes here */    
+    endwhile;
+    wp_reset_postdata();
+  endif;
+  ?>
+</div>
+
+<?php get_footer(); ?>
